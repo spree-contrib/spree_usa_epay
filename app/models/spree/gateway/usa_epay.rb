6d18d374ec634525bfb5bd67e735e0fbe373ff7e
Ryan Bigg <radarlistener@gmail.com>
2012-05-21 15:09:15 +1000

    Payment#gateway_options never took an argument

--- app/models/spree/gateway/usa_epay.rb
+++ app/models/spree/gateway/usa_epay.rb
@@ -19,7 +19,7 @@ module Spree
       gateway_options = if creditcard.respond_to?(:gateway_options)
         creditcard.gateway_options(payment)
       else
-        payment.send(:gateway_options, payment)
+        payment.send(:gateway_options)
       end
 
       if creditcard.gateway_customer_profile_id.nil?
